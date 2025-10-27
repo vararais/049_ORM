8a1498fc1df81c67c947906980cbeadb83eebdd1
vararais <varadigmaisnann@gmail.com>
2025-10-27 12:34:44 +0700

    Perbaikan dan penambahan fungsi App

--- index.js
+++ index.js
@@ -22,3 +22,53 @@ db.sequelize.sync()
         .catch((err) => {
             console.log(err);
         });
+
+app.post('/komik', async(req, res) => {
+    const data = req.body;
+    try{
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch(err){
+        res.send(err);
+    }
+});
+
+app.get('/komik', async(req, res) => {
+    try{
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch(err){
+        res.send(err);
+    }
+});
+
+app.put('/komik/:id', async(req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try{
+        const komik = await db.Komik.findByPk(id);
+        if(!komik){
+            return res.status(404).send({message: 'Komik tidak tersedia'});
+        }
+        await komik.update(data);
+        res.send({message: "Komik berhasil diupdate", komik});
+    } catch(err){
+        res.status(500).send(err);
+    }
+});
+
+app.delete('/komik/:id', async(req, res) => {
+    const id = req.params.id;
+
+    try{
+        const komik = await db.Komik.findByPk(id);
+        if(!komik){
+            return res.status(404).send({message: 'Komik tidak tersedia'});
+        }
+        await komik.destroy();
+        res.send({message: "Komik berhasil dihapus"});
+    } catch(err){
+        res.status(500).send(err);
+    }
+});
