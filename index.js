cdf9eadfa2a9734ae13194517c45ec872c2a9813
vararais <varadigmaisnann@gmail.com>
2025-10-27 12:05:42 +0700

    ubahan index.js

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.listen(PORT, () => {
     console.log('Server started on port 3000');
 });
 
-db.sequelize.sync().then((result) => {
-    app
-        .listen(3000, () => {
-            console.log('Server started');
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+                console.log('Server started');
+            })
         })
         .catch((err) => {
             console.log(err);
         });
-});
